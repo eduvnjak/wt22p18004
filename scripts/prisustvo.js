70763b9700d5747d3737b0da8539785e4e105088
Eman <eduvnjak1@etf.unsa.ba>
2022-11-24 00:54:31 +0100

    Iz podataka preuzet naziv predmeta

--- scripts/prisustvo.js
+++ scripts/prisustvo.js
@@ -1,54 +1,57 @@
-let div = document.getElementById("tabela_container");
-console.log("TU");
-//instanciranje
-let prisustvo = TabelaPrisustvo(div,
+const podaci = {
+    "studenti": [{
+        "ime": "Neko Nekic",
+        "index": 12345
+    },
     {
-        "studenti": [{
-            "ime": "Neko Nekic",
-            "index": 12345
-        },
-        {
-            "ime": "Drugi Neko",
-            "index": 12346
-        },
-        {
-            "ime": "Neko 2",
-            "index": 12347
-        }
-        ],
-        "prisustva": [{
-            "sedmica": 13,
-            "predavanja": 2,
-            "vjezbe": 1,
-            "index": 12345
-        },
-        {
-            "sedmica": 13,
-            "predavanja": 2,
-            "vjezbe": 2,
-            "index": 12346
-        },
-        {
-            "sedmica": 14,
-            "predavanja": 2,
-            "vjezbe": 0,
-            "index": 12345
-        },
-        {
-            "sedmica": 14,
-            "predavanja": 0,
-            "vjezbe": 0,
-            "index": 12346
-        },
-        {
-            "sedmica": 15,
-            "predavanja": 2,
-            "vjezbe": 3,
-            "index": 12346
-        }
-        ],
-        "predmet": "Razvoj mobilnih aplikacija",
-        "brojPredavanjaSedmicno": 3,
-        "brojVjezbiSedmicno": 5
+        "ime": "Drugi Neko",
+        "index": 12346
+    },
+    {
+        "ime": "Neko 2",
+        "index": 12347
     }
-);
+    ],
+    "prisustva": [{
+        "sedmica": 13,
+        "predavanja": 2,
+        "vjezbe": 1,
+        "index": 12345
+    },
+    {
+        "sedmica": 13,
+        "predavanja": 2,
+        "vjezbe": 2,
+        "index": 12346
+    },
+    {
+        "sedmica": 14,
+        "predavanja": 2,
+        "vjezbe": 0,
+        "index": 12345
+    },
+    {
+        "sedmica": 14,
+        "predavanja": 0,
+        "vjezbe": 0,
+        "index": 12346
+    },
+    {
+        "sedmica": 15,
+        "predavanja": 2,
+        "vjezbe": 3,
+        "index": 12346
+    }
+    ],
+    "predmet": "Razvoj mobilnih aplikacija",
+    "brojPredavanjaSedmicno": 3,
+    "brojVjezbiSedmicno": 5
+}
+let div = document.getElementById("tabela_container");
+//instanciranje
+let prisustvo = TabelaPrisustvo(div, podaci);
+
+for (const div of document.getElementById("info_predmet").childNodes) {
+    div.textContent = "";
+}
+document.getElementById("naziv_predmeta").textContent = podaci.predmet;
