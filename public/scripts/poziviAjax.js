d58bcda471d5271d5cdce67cec8234388b84ca18
Eman <eduvnjak1@etf.unsa.ba>
2023-01-05 02:29:59 +0100

    Implementiran pregled prisustva po predmetima za nastavnika

--- public/scripts/poziviAjax.js
+++ public/scripts/poziviAjax.js
@@ -4,7 +4,37 @@ const PoziviAjax = (() => {
     // svaki callback kao parametre ima error i data, error je null ako je status 200 i data je tijelo odgovora
     // ako postoji greška poruka se prosljeđuje u error parametar callback-a, a data je tada null
     function impl_getPredmet(naziv, fnCallback) {
+        var ajax = new XMLHttpRequest();
 
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4) {
+                if (ajax.status == 200) {
+                    fnCallback(null, JSON.parse(ajax.response));
+                } else {
+                    fnCallback(JSON.parse(ajax.response), null)
+                }
+            }
+        }
+
+        ajax.open("GET", `http://localhost:3000/predmeti/${naziv}`, true);
+        ajax.send();
+    }
+    // vraća listu predmeta za loginovanog nastavnika ili grešku da nastavnik nije loginovan
+    function impl_getPredmeti(fnCallback) {
+        var ajax = new XMLHttpRequest();
+
+        ajax.onreadystatechange = function () {
+            if (ajax.readyState == 4) {
+                if (ajax.status == 200) {
+                    fnCallback(null, JSON.parse(ajax.response));
+                } else {
+                    fnCallback(JSON.parse(ajax.response), null)
+                }
+            }
+        }
+
+        ajax.open("GET", "http://localhost:3000/predmeti", true);
+        ajax.send();
     }
     function impl_postLogin(username, password, fnCallback) {
         // console.log(`stiglo {username: ${username}, password: ${password}}`)
@@ -36,6 +66,7 @@ const PoziviAjax = (() => {
         postLogin: impl_postLogin,
         postLogout: impl_postLogout,
         getPredmet: impl_getPredmet,
+        getPredmeti: impl_getPredmeti,
         postPrisustvo: impl_postPrisustvo
     };
 })();
---
+++ public/scripts/predmeti.js
@@ -0,0 +1,29 @@
+const callback1 = function (error, data) {
+    if (error) {
+        document.getElementById("message").innerHTML = error.greska;
+    } else {
+        const meni = document.getElementById("meni");
+        for (const predmet of data.predmeti) {
+            const listItem = document.createElement("li");
+            listItem.innerText = predmet;
+            listItem.addEventListener("click", prikaziTabeluPrisustva.bind(null, predmet));
+            meni.children[0].appendChild(listItem);
+        }
+    }
+}
+const callback2 = function (error, data) {
+    if (error) {
+        document.getElementById("message").innerHTML = error.greska;
+    } else {
+        const tabelaContainer = document.getElementById("tabela_container");
+        if (data) {
+            TabelaPrisustvo(tabelaContainer, data);
+        } else {
+            tabelaContainer.innerHTML = "Nema podataka za ovaj predmet"
+        }
+    }
+}
+const prikaziTabeluPrisustva = (predmet) => {
+    PoziviAjax.getPredmet(predmet, callback2);
+}
+PoziviAjax.getPredmeti(callback1);
