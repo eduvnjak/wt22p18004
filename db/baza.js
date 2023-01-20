65b09a49e0b48f7eb6df17ae0da3bacd411cdc2d
Eman <eduvnjak1@etf.unsa.ba>
2023-01-20 17:28:50 +0100

    Dodan connection pool

--- db/baza.js
+++ db/baza.js
@@ -2,7 +2,13 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("wt22", "root", "password", {
     host: "localhost",
     dialect: "mysql",
-    logging: false
+    logging: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 const db = {};
 
